73ccb45fa522364a23870f756637be1d3418a94e
Davi Oliveira <78604613+Deivz@users.noreply.github.com>
2021-02-06 23:22:30 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,14 +1,21 @@
-function abrirMenu(x) {
-  if(x.classList.toggle("change")){
-  	document.documentElement.classList.add('menu-ativo');
-  } else{
-  	document.documentElement.classList.remove('menu-ativo');
-  }
+let menuAberto = document.querySelector(".abrir-menu");
+
+function toggle(event){
+	if(menuAberto.classList.contains("change")){
+		document.documentElement.classList.remove('menu-ativo');
+		menuAberto.classList.remove("change");
+	}else{
+		document.documentElement.classList.add('menu-ativo');
+		menuAberto.classList.add("change");
+	}
 }
+
+menuAberto.addEventListener("click", toggle, false);
 
 document.documentElement.onclick = function(event) {
     if (event.target === document.documentElement) {
         document.documentElement.classList.remove('menu-ativo');
+        menuAberto.classList.remove("change");
     }
-};
+}
 
