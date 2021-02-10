ecc636a938cf985777a35f6228781dde1b047d39
Davi Oliveira <78604613+Deivz@users.noreply.github.com>
2021-02-10 01:13:45 -0300

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,202 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('dado-errado')
+        input.parentElement.querySelector('.icon-erro').innerHTML = ""
+        input.parentElement.querySelector('.icon-ok').innerHTML = "&#9745"
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('dado-errado')
+        input.parentElement.querySelector('.icon-ok').innerHTML = ""
+        input.parentElement.querySelector('.icon-erro').innerHTML = "&#9940"
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.',
+        patternMismatch: 'Seu nome não pode possuir números!'
+    },
+    email: {
+        valueMissing: 'O campo de e-mail não pode estar vazio.',
+        customError: 'O e-mail digitado não é válido.',
+        patternMismatch: 'Por favor, digite um e-mail válido!'
+    },
+    confirmaEmail: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        customError: 'O email digitado não coincide com o anterior.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+        customError: 'O CPF digitado não é válido.' 
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP deve conter 8 dígitos e apenas números!',
+        customError: "Não foi possível encontrar o CEP informado."
+    },
+    logradouro: {
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo de cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo de estado não pode estar vazio.'
+    }
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input),
+    emailConfirma:input => validaEmailConfirma(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+    
+    return mensagem
+}
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
+
+function validaCPF(input) {
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!validarCPF(cpfFormatado)) {
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function validarCPF(cpf) {		
+	if(cpf == '') return false;	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+			return false;		
+	// Valida 1o digito	
+	let add = 0;
+    let rev = 0;	
+	for (let i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			return false;		
+	// Valida 2o digito	
+	add = 0;	
+	for (let i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+		return false;		
+	return true;   
+}
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if(!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
+}
+
+function validaEmailConfirma(input) {
+    const confirmarEmail = input.value
+    const emailDigitado = document.getElementById("email").value;
+    let mensagem = ''
+
+    if(confirmarEmail != emailDigitado) {
+        mensagem = 'O email digitado não coincide com o anterior.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
